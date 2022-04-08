52deeee5cad3366b5bb9cf1f82a7ad3181076193
abane1 <abanesebiane@outlook.fr>
2022-04-08 12:18:55 +0200

    JS

--- js/app.js
+++ js/app.js
@@ -1,30 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-(function(doc){
-    var scriptElm = doc.scripts[doc.scripts.length - 1];
-    var warn = ['[ionicons] Deprecated script, please remove: ' + scriptElm.outerHTML];
-  
-    warn.push('To improve performance it is recommended to set the differential scripts in the head as follows:')
-  
-    var parts = scriptElm.src.split('/');
-    parts.pop();
-    parts.push('ionicons');
-    var url = parts.join('/');
-  
-    var scriptElm = doc.createElement('script');
-    scriptElm.setAttribute('type', 'module');
-    scriptElm.src = url + '/ionicons.esm.js';
-    warn.push(scriptElm.outerHTML);
-    scriptElm.setAttribute('data-stencil-namespace', 'ionicons');
-    doc.head.appendChild(scriptElm);
-  
-    
-    scriptElm = doc.createElement('script');
-    scriptElm.setAttribute('nomodule', '');
-    scriptElm.src = url + '/ionicons.js';
-    warn.push(scriptElm.outerHTML);
-    scriptElm.setAttribute('data-stencil-namespace', 'ionicons');
-    doc.head.appendChild(scriptElm)
-    
-    console.warn(warn.join('\n'));
-  
-  })(document);
+    let ab = document.getElementsByName("password")[0];
+    let email = document.getElementsByName("email")[0];
+    let msg = document.querySelector("main form p:first-of-type")
+   
+    const User = {
+      mail: "abanesebiane@outlook.fr",
+      pwd: `abane`,
+
+    }
+
+    localStorage.setItem('mail', User.mail);
+    localStorage.setItem('password', User.pwd);
+    const emails = localStorage.getItem('mail', User.mail);
+    const mdp = localStorage.getItem('password', User.pwd);
+
+    let btn = document.querySelector("main form button");
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      ab.value == mdp && email.value == emails ? alert("Connexion réussi") : msg.innerHTML= `<p>E-Mail manquant ou invalide !</p>`;
+      console.log(btn);
+    })
+});
